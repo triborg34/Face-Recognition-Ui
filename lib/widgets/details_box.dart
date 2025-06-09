74206f7431e56ce1c5964f9b4ab627d2229801d3
triborg34 <you@example.com>
2025-06-09 11:00:09 +0330

    simpifed

--- lib/widgets/details_box.dart
+++ lib/widgets/details_box.dart
@@ -5,8 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class DetailsBox extends StatelessWidget {
-  const DetailsBox(
-      {super.key, required this.mController, required this.nController});
+  const DetailsBox({
+    super.key,
+    required this.mController,
+    required this.nController,
+  });
 
   final mainController mController;
   final networkController nController;
@@ -17,134 +20,134 @@ class DetailsBox extends StatelessWidget {
         ? SizedBox.shrink()
         : Expanded(
             child: AnimatedCrossFade(
-            duration: Duration(milliseconds: 500),
-            crossFadeState: mController.isPersonSelected.value
-                ? CrossFadeState.showFirst
-                : CrossFadeState.showSecond,
-            secondChild: SizedBox.shrink(),
-            firstChild: Container(
-              height: MediaQuery.of(context).size.height,
-              margin: EdgeInsets.all(15),
-              padding: EdgeInsets.all(25),
-              decoration: BoxDecoration(
+              duration: Duration(milliseconds: 500),
+              crossFadeState: mController.isPersonSelected.value
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              secondChild: SizedBox.shrink(),
+              firstChild: Container(
+                height: MediaQuery.of(context).size.height,
+                margin: EdgeInsets.all(15),
+                padding: EdgeInsets.all(25),
+                decoration: BoxDecoration(
                   border: Border.all(color: primaryColor),
                   color: Colors.black.withOpacity(0.4),
-                  borderRadius: BorderRadius.circular(15)),
-              child: Column(
-                textDirection: TextDirection.rtl,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Text(
-                      nController
-                          .personList[mController.globalIndex.value].name!,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CircleAvatar(
-                        radius: 60,
-                        backgroundImage: NetworkImage(nController
-                                    .personList[mController.globalIndex.value]
-                                    .croppedFrame!
-                                    .length ==
-                                0
-                            ? 'assets/images/unknown-person1.png'
-                            : 'http://127.0.0.1:8090/api/files/collection/${nController.personList[mController.globalIndex.value].id}/${nController.personList[mController.globalIndex.value].croppedFrame}'),
-                      ),
-                      Icon(Icons.arrow_forward),
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          nController
-                                    .personList[mController.globalIndex.value]
-                                    .croppedFrame!
-                                    .length ==
-                                0
-                            ? 'assets/images/unknown-person1.png' :
-                            "http://127.0.0.1:8090/api/files/known_face/${nController.knownList.firstWhere(
-                                  (p0) =>
-                                      p0.name ==
-                                      nController
-                                          .personList[
-                                              mController.globalIndex.value]
-                                          .name,
-                                ).id!}/${nController.knownList.firstWhere(
-                                  (p0) =>
-                                      p0.name ==
-                                      nController
-                                          .personList[
-                                              mController.globalIndex.value]
-                                          .name,
-                                ).image!}"),
-                        radius: 60,
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  CoustomRow(
-                    substring: nController
-                        .personList[mController.globalIndex.value].id!,
-                    title: "ID",
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  CoustomRow(
-                      title: "Track_ID",
-                      substring: nController
-                          .personList[mController.globalIndex.value].trackId!),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  CoustomRow(
-                      title: "Gender",
-                      substring: nController
-                          .personList[mController.globalIndex.value].gender!),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  CoustomRow(
-                      title: "Age",
-                      substring: nController
-                          .personList[mController.globalIndex.value].age!),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  CoustomRow(
-                      title: "Confidnce",
-                      substring:
-                          "${nController.personList[mController.globalIndex.value].score}%"),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Expanded(
-                      child: Container(
-                    child: nController.personList[mController.globalIndex.value]
-                                .frame!.length ==
-                            0
-                        ? Center(child: Icon(Icons.person))
-                        : ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: Image.network(
-                              'http://127.0.0.1:8090/api/files/collection/${nController.personList[mController.globalIndex.value].id}/${nController.personList[mController.globalIndex.value].frame}',
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: primaryColor),
-                        borderRadius: BorderRadius.circular(15)),
-                  ))
-                ],
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  textDirection: TextDirection.rtl,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildPersonName(),
+                    SizedBox(height: 15),
+                    _buildAvatarComparison(),
+                    SizedBox(height: 15),
+                    _buildPersonDetails(),
+                    SizedBox(height: 15),
+                    _buildFullImage(),
+                  ],
+                ),
               ),
             ),
-          )));
+          ));
+  }
+
+  Widget _buildPersonName() {
+    final person = nController.personList[mController.globalIndex.value];
+    return Center(
+      child: Text(
+        person.name!,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+    );
+  }
+
+  Widget _buildAvatarComparison() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildDetectedAvatar(),
+        Icon(Icons.arrow_forward),
+        _buildKnownAvatar(),
+      ],
+    );
+  }
+
+  Widget _buildDetectedAvatar() {
+    final person = nController.personList[mController.globalIndex.value];
+    final hasImage = person.croppedFrame?.isNotEmpty ?? false;
+    
+    return CircleAvatar(
+      radius: 60,
+      backgroundImage: NetworkImage(
+        hasImage
+            ? 'http://127.0.0.1:8090/api/files/collection/${person.id}/${person.croppedFrame}'
+            : 'assets/images/unknown-person1.png',
+      ),
+    );
+  }
+
+  Widget _buildKnownAvatar() {
+    final person = nController.personList[mController.globalIndex.value];
+    final hasImage = person.croppedFrame?.isNotEmpty ?? false;
+    
+    if (!hasImage) {
+      return CircleAvatar(
+        radius: 60,
+        backgroundImage: NetworkImage('assets/images/unknown-person1.png'),
+      );
+    }
+
+    final knownPerson = nController.knownList.firstWhere(
+      (p) => p.name == person.name,
+    );
+
+    return CircleAvatar(
+      radius: 60,
+      backgroundImage: NetworkImage(
+        'http://127.0.0.1:8090/api/files/known_face/${knownPerson.id}/${knownPerson.image}',
+      ),
+    );
+  }
+
+  Widget _buildPersonDetails() {
+    final person = nController.personList[mController.globalIndex.value];
+    
+    return Column(
+      children: [
+        CoustomRow(title: "ID", substring: person.id!),
+        SizedBox(height: 10),
+        CoustomRow(title: "Track_ID", substring: person.trackId!),
+        SizedBox(height: 10),
+        CoustomRow(title: "Gender", substring: person.gender!),
+        SizedBox(height: 10),
+        CoustomRow(title: "Age", substring: person.age!),
+        SizedBox(height: 10),
+        CoustomRow(title: "Confidence", substring: "${person.score}%"),
+      ],
+    );
+  }
+
+  Widget _buildFullImage() {
+    final person = nController.personList[mController.globalIndex.value];
+    final hasFrame = person.frame?.isNotEmpty ?? false;
+    
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: primaryColor),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: hasFrame
+            ? ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  'http://127.0.0.1:8090/api/files/collection/${person.id}/${person.frame}',
+                  fit: BoxFit.fill,
+                ),
+              )
+            : Center(child: Icon(Icons.person)),
+      ),
+    );
   }
 }
